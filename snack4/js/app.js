c977d68fa384c6fb16d8f2e0cbfd527f6884b9e1
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-07 17:38:12 +0200

    snack 4 done

--- snack4/js/app.js
+++ snack4/js/app.js
@@ -11,8 +11,8 @@ Due a list of students of a college, with the total of their votes
 ];
 
 1- we have to create some plates with their names capitalized. We need a new string array. Every element of the new array will be the name of the student but with all the letters capitalized.
-2- We have to create a new array with all the students that have a total of vows more than 70.
-3- We have to create a new array with all the students that have a total of vows more than 70 and id more tha 120.
+2- We have to create a new array with all the students that have a total of grades more than 70.
+3- We have to create a new array with all the students that have a total of grades more than 70 and id more than 120.
 */
 
 let students = [
@@ -54,9 +54,33 @@ let students = [
 
 ];
 
-
+//capitalized names
 const  capitalizedNames = students.map((student) => {
     return student.name.toUpperCase();
 });
 
 console.log(capitalizedNames);
+
+
+//grades more than 70
+const gradesMoreThan70 =  students.map((student) => {
+    if(student.grades > 70) {
+        return student.name;
+    } else {
+        return '';
+    }        
+})
+
+console.log(gradesMoreThan70);
+
+//grades more than 70 and id more than 120
+
+const gradesMoreThan70AndId120 =  students.map((student) => {
+    if(student.grades > 70 && student.id > 120) {
+        return student.name;
+    } else {
+        return '';
+    }
+});
+
+console.log(gradesMoreThan70AndId120);
